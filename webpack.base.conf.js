5a6fa0bc9de6f807015f04b2212819327b12da3c
BarryWin <slavon.v21@gmail.com>
2019-07-14 03:05:07 +0300

    header is ready

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -40,7 +40,13 @@ module.exports = {
       test: /\.js$/,
       loader: 'babel-loader',
       exclude: '/node_modules/'
-    },  {
+    }, {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]'
+      }
+    }, {
       test: /\.(png|jpg|gif|svg)$/,
       loader: 'file-loader',
       options: {
@@ -96,4 +102,4 @@ module.exports = {
       { from: `${PATHS.src}/fonts`, to: `${PATHS.dist}/fonts` }
     ])
   ],
-}
+};
