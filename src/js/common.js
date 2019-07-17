f65f7a10b9ce4f0fc9bf0cf46c2e6f1c5dd32072
BarryWin <slavon.v21@gmail.com>
2019-07-17 06:28:51 +0300

    half responsive

--- src/js/common.js
+++ src/js/common.js
@@ -20,9 +20,28 @@ let slider = document.getElementById("slider");
     slider.addEventListener("mouseover", () => mySwiper.autoplay.stop());
     slider.addEventListener("mouseout", () => mySwiper.autoplay.start());
 
+
 var swiper2 = new Swiper('.swiper-goods-container', {
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 'auto',
+    breakpointsInverse: true,
+    breakpoints: {
+        // when window width is >= 1500px
+        1500: {
+            slidesPerView: 4,
+            spaceBetween: 7
+        },
+        // when window width is >= 1100px
+        1140: {
+            slidesPerView: 3,
+            spaceBetween: 7
+        },
+        // when window width is <= 320px
+        880: {
+            slidesPerView: 2,
+            spaceBetween: 7
+        },
+    },
     spaceBetween: 7,
     navigation: {
         nextEl: '.swiper-button-next',
